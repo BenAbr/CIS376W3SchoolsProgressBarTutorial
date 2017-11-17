bd485b5f0d6cab96682f7d56666acceaec9ce228
BenAbr <31945456+BenAbr@users.noreply.github.com>
2017-11-16 18:31:20 -0600

    Main

---
+++ index.js
@@ -0,0 +1,28 @@
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function move1() {
+  var elem = document.getElementById("myBar1");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
